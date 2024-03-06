eb73e92a4d4e7149443bd8a6783ac0941da3c22f
Sarah Kouhail <kouhail.sarah@gmail.com>
2024-03-06 15:54:45 -0500

    adds comment over truck 2 for package 9

--- truck.py
+++ truck.py
@@ -20,6 +20,7 @@ class Truck:
 truck1 = Truck(id=1, maxPackages=16, speed=18, load=None, packages=[1, 6, 21, 19, 7, 13, 14, 15, 16, 20, 29, 30, 31, 34, 37, 40],
                miles=0.0, address="4001 South 700 East", departureTime=datetime.timedelta(hours=8))
 
+#package 9- correct address updates at 10:20
 truck2 = Truck(id=2, maxPackages=16, speed=18, load=None, packages=[3, 39, 9, 11, 12, 17, 18, 38, 5, 22, 23, 24, 25, 26, 32, 36],
                miles=0.0, address="4001 South 700 East", departureTime=datetime.timedelta(hours=10, minutes=20))
 
